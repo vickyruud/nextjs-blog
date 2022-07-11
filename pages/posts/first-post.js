d7be34da487580088e024fd8b2469425133a3489
vickyruud <aprajit.k@gmail.com>
2022-07-11 15:15:45 -0400

    fetch data from file

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import Layout from "../../components/layout";
 
 export default function FirstPost() {
@@ -8,12 +7,7 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>   
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+      <h1>First Post</h1>      
     </Layout>
   );
 }
